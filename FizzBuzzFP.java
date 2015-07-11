cf91bb7ae6b11136202deb946db3a2103cc1e5a5
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2015-07-11 20:23:18 +0900

    名前空間を変更

--- FizzBuzzFP.java
+++ FizzBuzzFP.java
@@ -1,23 +1,29 @@
 import java.util.stream.IntStream;
 
 public class FizzBuzzFP {
-  public static String fizzBuzz(final int n) {
-    final boolean isDivisibleBy3 = n % 3 == 0;
-    final boolean isDivisibleBy5 = n % 5 == 0;
-    if (isDivisibleBy3 && isDivisibleBy5) {
-      return "Fizz Buzz";
-    } else if (isDivisibleBy3) {
-      return "Fizz";
-    } else if (isDivisibleBy5) {
-      return "Buzz";
-    } else {
-      return String.valueOf(n);
+  public static class FizzBuzz {
+    public static String fizzBuzz(final int n) {
+      if (n < 1) {
+        throw new IllegalArgumentException("n cannot be less than 1");
+      }
+
+      final boolean isDivisibleBy3 = n % 3 == 0;
+      final boolean isDivisibleBy5 = n % 5 == 0;
+      if (isDivisibleBy3 && isDivisibleBy5) {
+        return "Fizz Buzz";
+      } else if (isDivisibleBy3) {
+        return "Fizz";
+      } else if (isDivisibleBy5) {
+        return "Buzz";
+      } else {
+        return String.valueOf(n);
+      }
     }
   }
 
   public static void main(final String[] args) {
     IntStream.rangeClosed(1, 30)
-      .mapToObj(FizzBuzzFP::fizzBuzz)
+      .mapToObj(FizzBuzz::fizzBuzz)
       .forEach(System.out::println);
   }
 }
--- FizzBuzzOOP.java
+++ FizzBuzzOOP.java
@@ -1,15 +1,21 @@
 public class FizzBuzzOOP {
-  public static String fizzBuzz(final int n) {
-    final boolean isDivisibleBy3 = n % 3 == 0;
-    final boolean isDivisibleBy5 = n % 5 == 0;
-    if (isDivisibleBy3 && isDivisibleBy5) {
-      return "Fizz Buzz";
-    } else if (isDivisibleBy3) {
-      return "Fizz";
-    } else if (isDivisibleBy5) {
-      return "Buzz";
-    } else {
-      return String.valueOf(n);
+  public static class FizzBuzz {
+    public static String fizzBuzz(final int n) {
+      if (n < 1) {
+        throw new IllegalArgumentException("n cannot be less than 1");
+      }
+
+      final boolean isDivisibleBy3 = n % 3 == 0;
+      final boolean isDivisibleBy5 = n % 5 == 0;
+      if (isDivisibleBy3 && isDivisibleBy5) {
+        return "Fizz Buzz";
+      } else if (isDivisibleBy3) {
+        return "Fizz";
+      } else if (isDivisibleBy5) {
+        return "Buzz";
+      } else {
+        return String.valueOf(n);
+      }
     }
   }
 
@@ -28,7 +34,7 @@ class FizzBuzzer {
 
   public void say(final int t) {
     for (int i = 0; i < t; i++) {
-      System.out.println(FizzBuzzOOP.fizzBuzz(n));
+      System.out.println(FizzBuzz.fizzBuzz(n));
       n++;
     }
   }
