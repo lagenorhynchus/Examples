6663ee22b76b472ad9b217eaf5db6ee68ca7aff8
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2015-08-21 23:45:32 +0900

    レイアウトを修正

--- fizz_buzz_fp.clj
+++ fizz_buzz_fp.clj
@@ -4,12 +4,12 @@
   (let [divisible-by-3? (zero? (mod n 3))
         divisible-by-5? (zero? (mod n 5))]
     (cond
-      (< n 1)              (throw (IllegalArgumentException. "n cannot be less than 1"))
+      (< n 1)               (throw (IllegalArgumentException. "n cannot be less than 1"))
       (and divisible-by-3?
            divisible-by-5?) "Fizz Buzz"
       divisible-by-3?       "Fizz"
       divisible-by-5?       "Buzz"
-      :else                (str n))))
+      :else                 (str n))))
 
 ;; pattern 1
 (doall (map (comp println fizz-buzz) (range 1 (inc 30))))
