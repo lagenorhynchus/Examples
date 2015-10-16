77aec2c0ec24e16e79dc35544938930945b9f9e2
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2015-10-16 17:10:47 +0900

    新規作成

---
+++ RunLengthEncoding.scala
@@ -0,0 +1,12 @@
+object RunLengthEncoding {
+  def rle(s: String): String = {
+    s.reverse.foldLeft(List[(Char, Int)]()) { (ts, c) =>
+      ts match {
+        case x :: xs if x._1 == c => (c, x._2 + 1) :: xs
+        case _ => (c, 1) :: ts
+      }
+    }.foldLeft(new StringBuilder) { (sb, t) =>
+      sb.append(t._1).append(t._2)
+    }.mkString("")
+  }
+}
