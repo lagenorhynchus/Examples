ba801b3fccfe3182f48a77553733821700e961ab
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2015-10-16 17:51:01 +0900

    doctestを追加

--- RunLengthEncoding.hs
+++ RunLengthEncoding.hs
@@ -2,5 +2,11 @@ module RunLengthEncoding where
 
 import Data.List (group)
 
+-- | ランレングス符号化
+--
+-- >>> rle "AAAABBCCCA"
+-- "A4B2C3A1"
+-- >>> rle ""
+-- ""
 rle :: String -> String
 rle = concatMap (\s -> head s : show (length s)) . group
