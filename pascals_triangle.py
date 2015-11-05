50c4fb8e8e606e3617bf5988eebf1bbae39fa057
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2015-11-05 16:58:59 +0900

    initial commit

---
+++ pascals_triangle.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+import sys
+
+from scipy.misc import comb
+
+
+def pascals_triangle(m):
+    for n in xrange(m):
+        print ('  ' * (m - n - 1) + ''.join('{0:>4}'.format(comb(n, k, 1))
+                                            for k in xrange(n + 1)))
+
+
+if __name__ == '__main__':
+    n = 5
+    if len(sys.argv) == 2:
+        n = int(sys.argv[1])
+    pascals_triangle(n)
